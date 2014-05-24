dde5f1cb760ab83fec7e83e1177966e616183f74
Horacio Fernandes <horacioalexandre.fernandes@gmail.com>
2014-05-24 14:25:07 -0300

    add rails backend

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiogoPoll::Application.config.secret_key_base = '19a0592a5afe52dc39b0e69c1f3ddc792968abdc60bdb1463c80bc660e880961d9583e5fb9bdf9820c49848a490f1ab86039cb33bc7c36ce3edd4a058e5a5e85'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiogoPoll::Application.config.session_store :cookie_store, key: '_diogo-poll_session'
